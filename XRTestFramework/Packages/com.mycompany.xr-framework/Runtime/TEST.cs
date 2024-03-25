452f48aa8f2568c0d363c8e770b53390f584abdd
Mikhail Pavliukov <mikhail_pavliukov@epam.com>
2024-03-25 18:00:32 +0400

    add

---
+++ XRTestFramework/Packages/com.mycompany.xr-framework/Runtime/TEST.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace XRFramework
+{
+    public class TEST : MonoBehaviour
+    {
+        public void Hello()
+        {
+            Debug.Log("HELLO");
+        }
+    }
+}
